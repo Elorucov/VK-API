534101550de839ae0beabf5e6f7e57a04dc7f014
Elorucov <elorucov@live.ru>
2020-08-06 22:08:28 +0400

    polls.getBackgrounds method.

---
+++ ELOR.VKAPILib/Methods/PollsMethods.cs
@@ -0,0 +1,20 @@
+﻿using ELOR.VKAPILib.Attributes;
+using ELOR.VKAPILib.Objects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ELOR.VKAPILib.Methods {
+    [Section("polls")]
+    public class PollsMethods : MethodsSectionBase {
+        internal PollsMethods(VKAPI api) : base(api) { }
+
+        /// <summary>Return default backgrounds for polls.</summary>
+        [Method("getBackgrounds")]
+        public async Task<List<PollBackground>> GetBackgroundsAsync() {
+            Dictionary<string, string> parameters = new Dictionary<string, string>();
+            return await API.CallMethodAsync<List<PollBackground>>(this, parameters);
+        }
+    }
+}
