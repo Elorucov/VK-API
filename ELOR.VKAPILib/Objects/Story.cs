d7a2b3f58ad23ac26235f1a6e690b78dfa456687
Elorucov <elorucov@live.ru>
2021-05-01 10:46:27 +0400

    Changes in story object.

--- ELOR.VKAPILib/Objects/Story.cs
+++ ELOR.VKAPILib/Objects/Story.cs
@@ -27,12 +27,81 @@ namespace ELOR.VKAPILib.Objects {
         public Uri Uri { get { return new Uri(Url); } }
     }
 
+    public class ClickableStickerAreaPoints {
+        [JsonProperty("x")]
+        public int X { get; set; }
+
+        [JsonProperty("y")]
+        public int Y { get; set; }
+    }
+
+    public class ClickableSticker {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("type")]
+        public string Type { get; set; }
+
+        [JsonProperty("mention")]
+        public string Mention { get; set; }
+
+        [JsonProperty("hashtag")]
+        public string Hashtag { get; set; }
+
+        [JsonProperty("place_id")]
+        public int PlaceId { get; set; }
+
+        [JsonProperty("market_item")]
+        public Market MarketItem { get; set; }
+
+        [JsonProperty("poll")]
+        public Poll Poll { get; set; }
+
+        [JsonProperty("sticker_id")]
+        public int StickerId { get; set; }
+
+        [JsonProperty("sticker_pack_id")]
+        public int StickerPackId { get; set; }
+
+        [JsonProperty("link_object")]
+        public Link LinkObject { get; set; }
+
+        [JsonProperty("post_id")]
+        public int PostId { get; set; }
+
+        [JsonProperty("post_owner_id")]
+        public int PostOwnerId { get; set; }
+
+        [JsonProperty("owner_id")]
+        public int OwnerId { get; set; }
+
+        [JsonProperty("story_id")]
+        public int StoryId { get; set; }
+
+        [JsonProperty("clickable_area")]
+        public List<ClickableStickerAreaPoints> ClickableArea { get; set; }
+    }
+
+    public class ClickableStickersInfo {
+        [JsonProperty("original_height")]
+        public int OriginalHeight { get; set; }
+
+        [JsonProperty("original_width")]
+        public int OriginalWidth { get; set; }
+
+        [JsonProperty("clickable_stickers")]
+        public List<ClickableSticker> ClickableStickers { get; set; }
+    }
+
     public class Story : AttachmentBase {
         [JsonIgnore]
         public new string ObjectType { get { return "story"; } }
 
         [JsonProperty("can_see")]
         public int CanSee { get; set; }
+
+        [JsonProperty("can_like")]
+        public bool CanLike { get; set; }
 
         [JsonProperty("can_share")]
         public int CanShare { get; set; }
@@ -63,5 +132,8 @@ namespace ELOR.VKAPILib.Objects {
 
         [JsonProperty("video")]
         public Video Video { get; set; }
+
+        [JsonProperty("clickable_stickers")]
+        public ClickableStickersInfo ClickableStickers { get; set; }
     }
 }
